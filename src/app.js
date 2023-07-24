e0f5ef4eedcc4d849d075b5eafdb799b75ce77a7
eldarlrd <eldarlrd@pm.me>
2023-07-24 15:39:00 +0400

    feat: PvP Mode

--- src/app.js
+++ src/app.js
@@ -4,14 +4,29 @@ import Header from './components/header';
 import Footer from './components/footer';
 import 'tachyons';
 
+const victoryConditions = [
+  // Horizontal
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  // Vertical
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  // Diagonal
+  [1, 5, 9],
+  [3, 5, 7]
+]
+
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      actor: 'X',
+      actor: '',
       winner: '',
       mode: 'PvP',
-      gridBoxes: []
+      gridBoxes: [],
+      gameOver: false
     }
   }
 
@@ -19,16 +34,73 @@ export default class App extends Component {
     this.gridLoop()
   }
 
-  switchActor() {
+  switchActor(tile) {
+    if (tile.innerText) return;
     this.setState({
       actor: this.state.actor === 'X' ? 'O' : 'X',
     })
   }
 
   switchMode(state) {
+    if (this.state.mode === state) return;
+    this.restart();
     this.setState({
       mode: state,
     })
+  }
+
+  checkConditions() {
+    const tiles = document.getElementsByClassName('board');
+    this.checkVictory(tiles);
+    this.checkDraw(tiles);
+  }
+
+  checkVictory(tiles) {
+    const victoryState = victoryConditions.some(condition => {
+      return condition.every(i => {
+        return tiles[i - 1].innerText === this.state.actor;
+      })
+    });
+
+    if (victoryState) {
+      this.setState({
+        winner: this.state.actor,
+        gameOver: true
+      });
+    }
+  }
+
+  checkDraw(tiles) {
+    const drawState = [...tiles].every(tile => {
+      return tile.innerText.includes('X') || tile.innerText.includes('O')
+    });
+
+    if (drawState) {
+      this.setState({
+        winner: 'None',
+        gameOver: true
+      })
+    }
+  }
+
+  addActor(tile) {
+    if (this.state.gameOver) this.restart();
+    else if (tile.innerText) return;
+    else {
+      this.switchActor(tile);
+      tile.innerText = this.state.actor;
+      tile.classList.add(tile.innerText === 'X' ? 'dark-red' : 'blue')
+      this.checkConditions();
+    }
+  }
+
+  restart() {
+    this.setState({
+      actor: '',
+      winner: '',
+      gridBoxes: [],
+      gameOver: false
+    }); this.gridLoop()
   }
 
   // Populate the Board
@@ -36,10 +108,10 @@ export default class App extends Component {
     let arr =[];
     for (let i = 1; i <= 9; i++) {
       arr.push(
-        h('div', { key: i, id: i, onClick: e => {this.switchActor(); (console.log(e.target.id))}, style: {'height': 'calc(100% / 3)'}, class: 'board ba bw1 b--dark-blue pointer fl w-third flex items-center justify-center'}, [
-          h('span', { class: 'b flex items-center justify-center',
-          style: { 'font-family': 'Patrick Hand, cursive', 'font-size': '5em' } }, '')
-        ])
+        h('div', { key: i, id: i, onClick: e => this.addActor(e.target),
+        style: {'height': 'calc(100% / 3)', 'font-family': 'Patrick Hand, cursive', 'font-size': '5em'},
+        class: 'b board ba bw1 b--dark-blue pointer fl w-third flex items-center justify-center'},
+        '')
       )
     } this.setState({ gridBoxes: arr });
   }
@@ -57,16 +129,17 @@ export default class App extends Component {
               h('i', {class: 'fa-solid fa-user h1 w1 mr2'}), ' PvP'
             ])
           ]),
-          this.state.winner !== '' ? h('div', [
-            'Player ', h('span', {style: {'font-family': 'Patrick Hand, cursive'}, class: this.state.winner === 'X' ? 'b dark-red' : 'b blue'}, this.state.winner), ' wins!'
-          ]) : h('div', [
+          this.state.winner === '' ? h('div', {style: {'height': '28px'}}, [
             this.state.mode === 'PvP' ? 'Player vs Player' : 'Player vs Computer'
+          ]) : this.state.winner === 'None' ? h('div', {style: {'height': '28px'}}, "It's a Draw!")
+          : h('div', [
+            'Player ', h('span', {style: {'font-family': 'Patrick Hand, cursive', 'height': '28px'}, class: this.state.winner === 'X' ? 'b dark-red' : 'b blue'}, this.state.winner), ' wins!'
           ])
         ]),
         h('section#gameBoard', {style: {'width': '21em', 'height': '21em'}, class: 'mb3 cf'}, [
           this.state.gridBoxes
         ]),
-        h('button', {class: 'avenir bw0 outline-0 bg-animate pointer bg-transparent dark-blue b mt4 f3 br-pill pl3 pr3 pt2 pb2 ba bw1 b--dark-blue'}, [
+        h('button', {onClick: () => this.restart(), class: 'avenir bw0 outline-0 bg-animate pointer bg-transparent dark-blue b mt4 f3 br-pill pl3 pr3 pt2 pb2 ba bw1 b--dark-blue'}, [
           h('i', {class: 'fa-solid fa-repeat mr2'}), 'Restart'
         ])
       ]),
