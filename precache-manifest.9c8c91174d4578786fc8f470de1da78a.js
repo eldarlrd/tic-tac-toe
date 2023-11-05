7c091372909c37b2402d94fed17395994a3babe4
eldarlrd <eldarlrd@pm.me>
2023-11-05 18:16:45 +0400

    build: Deployment

---
+++ precache-manifest.9c8c91174d4578786fc8f470de1da78a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ef7e18c952dc90867ae",
+    "url": "/tic-tac-toe/static/js/main.2ef7e18c.chunk.js"
+  },
+  {
+    "revision": "837ee52cce0c84c781f2",
+    "url": "/tic-tac-toe/static/js/runtime~main.837ee52c.js"
+  },
+  {
+    "revision": "3479b521a4f8c4342686",
+    "url": "/tic-tac-toe/static/css/2.6327185f.chunk.css"
+  },
+  {
+    "revision": "3479b521a4f8c4342686",
+    "url": "/tic-tac-toe/static/js/2.3479b521.chunk.js"
+  },
+  {
+    "revision": "4b5b265aea5dbd741c855a9235b99f28",
+    "url": "/tic-tac-toe/static/media/scribble.4b5b265a.ogg"
+  },
+  {
+    "revision": "42c485ad13ac7872d64361caf3b33e7a",
+    "url": "/tic-tac-toe/static/media/game-over.42c485ad.ogg"
+  },
+  {
+    "revision": "548762d246a344b47b2dd782cb009657",
+    "url": "/tic-tac-toe/static/media/paper-flip.548762d2.ogg"
+  },
+  {
+    "revision": "02f4a1ce1130bff7ef66af1db6f0c394",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tic-tac-toe/precache-manifest.12412524a51087cc1517382519225565.js"
+  "/tic-tac-toe/precache-manifest.9c8c91174d4578786fc8f470de1da78a.js"
 );
 
 workbox.clientsClaim();
